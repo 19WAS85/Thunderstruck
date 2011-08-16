7a86114b1eb2edc92a01e8fdbef887c77f8dbd33
Wagner Andrade <wagner.andradesilva@gmail.com>
2011-08-16 11:21:01 -0300

    First commit.

---
+++ Encanado/DataContext.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace Encanado.Access
+{
+    public class DataContext : IDisposable
+    {
+        private SqlConnection _connection;
+        private SqlTransaction _transaction;
+
+        static DataContext()
+        {
+            DefaultConnectionStringName = "Default";
+        }
+
+        public DataContext() : this(null) { }
+
+        public DataContext(string connectionString)
+        {
+            if (String.IsNullOrEmpty(connectionString))
+            {
+                connectionString = ConfigurationManager.ConnectionStrings[DefaultConnectionStringName].ConnectionString;
+            }
+
+            _connection = new SqlConnection(connectionString);
+        }
+
+        public static string DefaultConnectionStringName { get; set; }
+
+        public SqlDataReader Query(string query, object queryParams = null)
+        {
+            return CommandToExecute(query, queryParams).ExecuteReader();
+        }
+
+        public int Execute(string command, object commandParams = null)
+        {
+            return CommandToExecute(command, commandParams).ExecuteNonQuery();
+        }
+
+        public int GetIdentity(string command, object commandParams = null)
+        {
+            var identityQuery = String.Concat(command, "; SELECT SCOPE_IDENTITY()");
+            return GetValue<int>(identityQuery, commandParams);
+        }
+
+        public T GetValue<T>(string query, object queryParams = null)
+        {
+            var value = CommandToExecute(query, queryParams).ExecuteScalar();
+
+            if (value is DBNull) return default(T);
+            else return (T) Convert.ChangeType(value, typeof(T));
+        }
+
+        public object GetValue(string query, object queryParams = null)
+        {
+            return GetValue<object>(query, queryParams);
+        }
+
+        public void Commit()
+        {
+            _transaction.Commit();
+        }
+
+        public void Dispose()
+        {
+            _connection.Close();
+        }
+
+        private SqlCommand CommandToExecute(string query, object objectParameters)
+        {
+            try
+            {
+                OpenConnection();
+            }
+            catch (Exception err)
+            {
+                var message = String.Format("Error to open connection: {0}", err.Message);
+                throw new DataException(message, err);
+            }
+
+            var command = new SqlCommand { CommandText = query, Connection = _connection, Transaction = _transaction };
+            if (objectParameters != null) command.AddParameters(objectParameters);
+
+            return command;
+        }
+
+        private void OpenConnection()
+        {
+            if (_connection.State == ConnectionState.Closed)
+            {
+                _connection.Open();
+                _transaction = _connection.BeginTransaction();
+            }
+        }
+    }
+}
---
+++ Encanado/DataExtensions.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Encanado.Access
+{
+    public static class DataExtensions
+    {
+        #region SqlDataReader
+
+        public static T[] ToArray<T>(this SqlDataReader reader) where T : new()
+        {
+            var list = new List<T>();
+            var properties = typeof(T).GetProperties();
+
+            while (reader.Read())
+            {
+                var item = new T();
+
+                foreach (var p in properties)
+                {
+                    try
+                    {
+                        var ordinal = reader.GetOrdinal(p.Name);
+                        var value = reader.GetValue(ordinal);
+                        var valueTyped = Convert.ChangeType(value, p.PropertyType);
+
+                        p.SetValue(item, valueTyped, null);
+                    }
+                    catch (IndexOutOfRangeException) { continue; }
+                }
+
+                list.Add(item);
+            }
+
+            reader.Close();
+
+            return list.ToArray();
+        }
+
+        #endregion
+
+        #region SqlCommand
+
+        public static void AddParameters(this SqlCommand command, object objectParameters)
+        {
+            foreach (var p in objectParameters.GetType().GetProperties())
+            {
+                var parameterName = String.Concat("@", p.Name);
+
+                if (command.CommandText.Contains(parameterName))
+                {
+                    var parameterValue = p.GetValue(objectParameters, null) ?? DBNull.Value;
+
+                    command.Parameters.AddWithValue(parameterName, parameterValue);
+                }
+            }
+        }
+
+        #endregion
+
+        #region DataContext
+
+        public static T[] All<T>(this DataContext data, string query, object queryParams = null) where T : new()
+        {
+            return data.Query(query, queryParams).ToArray<T>();
+        }
+
+        public static T First<T>(this DataContext data, string query, object queryParams = null) where T : new()
+        {
+            return data.All<T>(query, queryParams).First();
+        }
+
+        #endregion
+    }
+}
