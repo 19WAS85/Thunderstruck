84dcaec6a62b67052ff3b0b343821bca61e5adf4
Brian DeMarzo <bdemarzo@innovatix.com>
2015-01-23 10:58:47 -0500

    changed to spaces for indenting

--- Thunderstruck/Provider/ProviderFactory.cs
+++ Thunderstruck/Provider/ProviderFactory.cs
@@ -8,84 +8,84 @@ using Thunderstruck.Provider.Common;
 
 namespace Thunderstruck.Provider
 {
-	public class ProviderFactory
-	{
-		public static Func<string, IDataProvider> CustomProvider { get; set; }
+    public class ProviderFactory
+    {
+        public static Func<string, IDataProvider> CustomProvider { get; set; }
 
-		public static Func<string, IDbConnection> ConnectionFactory { get; set; }
+        public static Func<string, IDbConnection> ConnectionFactory { get; set; }
 
-		private static Dictionary<string, Type> Providers;
+        private static Dictionary<string, Type> Providers;
 
-		static ProviderFactory()
-		{
-			Providers = new Dictionary<string, Type>();
+        static ProviderFactory()
+        {
+            Providers = new Dictionary<string, Type>();
 
-			AddProvider("System.Data.SqlClient", typeof(SqlProvider));
-			AddProvider("System.Data.OracleClient", typeof(OracleProvider));
-			AddProvider("MySql.Data.MySqlClient", typeof(MySqlProvider));
-		}
+            AddProvider("System.Data.SqlClient", typeof(SqlProvider));
+            AddProvider("System.Data.OracleClient", typeof(OracleProvider));
+            AddProvider("MySql.Data.MySqlClient", typeof(MySqlProvider));
+        }
 
-		public IDataProvider Create(ConnectionStringSettings settings, Transaction transactionMode)
-		{
-			var provider = ResolveDataProvider(settings.ProviderName);
-			provider.DbConnection = CreateConnection(settings.ProviderName, settings);
-			provider.TransactionMode = transactionMode;
-			return provider;
-		}
+        public IDataProvider Create(ConnectionStringSettings settings, Transaction transactionMode)
+        {
+            var provider = ResolveDataProvider(settings.ProviderName);
+            provider.DbConnection = CreateConnection(settings.ProviderName, settings);
+            provider.TransactionMode = transactionMode;
+            return provider;
+        }
 
-		public IDataProvider ResolveDataProvider(string providerName)
-		{
-			if (CustomProvider != null) return CustomProvider(providerName);
+        public IDataProvider ResolveDataProvider(string providerName)
+        {
+            if (CustomProvider != null) return CustomProvider(providerName);
 
-			if (Providers.ContainsKey(providerName))
-			{
-				return (IDataProvider)Activator.CreateInstance(Providers[providerName]);
-			}
+            if (Providers.ContainsKey(providerName))
+            {
+                return (IDataProvider)Activator.CreateInstance(Providers[providerName]);
+            }
 
-			var exceptionMessage = String.Format(
-				"Thunderstruck do not supports the '{0}' provider. Either add your provider using the ProviderFactory.AddProvider() method, or create and set a ProviderResolver.CustomProvider.",
-				providerName);
+            var exceptionMessage = String.Format(
+                "Thunderstruck do not supports the '{0}' provider. Either add your provider using the ProviderFactory.AddProvider() method, or create and set a ProviderResolver.CustomProvider.",
+                providerName);
 
-			throw new ThunderException(exceptionMessage);
-		}
+            throw new ThunderException(exceptionMessage);
+        }
 
-		private IDbConnection CreateConnection(string providerName, ConnectionStringSettings settings)
-		{
-			IDbConnection connection;
+        private IDbConnection CreateConnection(string providerName, ConnectionStringSettings settings)
+        {
+            IDbConnection connection;
 
-			if (ConnectionFactory != null) connection = ConnectionFactory(providerName);
-			else connection = DbProviderFactories.GetFactory(providerName).CreateConnection();
+            if (ConnectionFactory != null) connection = ConnectionFactory(providerName);
+            else connection = DbProviderFactories.GetFactory(providerName).CreateConnection();
 
-			connection.ConnectionString = settings.ConnectionString;
+            connection.ConnectionString = settings.ConnectionString;
 
-			return connection;
-		}
+            return connection;
+        }
 
-		public static void AddProvider(string providerName, Type providerType)
-		{
-			if (String.IsNullOrWhiteSpace(providerName))
-			{
-				throw new ArgumentException("Parameter 'providerName' can not be null and must contain data.", "providerName");
-			}
-			if (providerType == null)
-			{
-				throw new ArgumentNullException("providerType");
-			}
-			if (Providers.ContainsKey(providerName))
-			{
-				if (!Providers[providerName].Equals(providerType))
-				{
-					var message = String.Format(
-						"Attempting to add a provider named '{0}' of type '{1}', but an existing provider of type '{2}' already exists with that name.",
-						providerName, providerType.ToString(), Providers[providerName].ToString());
+        public static void AddProvider(string providerName, Type providerType)
+        {
+            if (String.IsNullOrWhiteSpace(providerName))
+            {
+                throw new ArgumentException("Parameter 'providerName' can not be null and must contain data.", "providerName");
+            }
+            if (providerType == null)
+            {
+                throw new ArgumentNullException("providerType");
+            }
+            if (Providers.ContainsKey(providerName))
+            {
+                if (!Providers[providerName].Equals(providerType))
+                {
+                    var message = String.Format(
+                        "Attempting to add a provider named '{0}' of type '{1}', but an existing provider of type '{2}' already exists with that name.",
+                        providerName, providerType.ToString(), Providers[providerName].ToString());
 
-					throw new ThunderException(message);
-				}
-			}
-			else
-			{
-				Providers.Add(providerName, providerType);
-			}
-		}
-	}
+                    throw new ThunderException(message);
+                }
+            }
+            else
+            {
+                Providers.Add(providerName, providerType);
+            }
+        }
+    }
 }
--- Thunderstruck/Runtime/DataReader.cs
+++ Thunderstruck/Runtime/DataReader.cs
@@ -7,124 +7,124 @@ using System.Reflection;
 
 namespace Thunderstruck.Runtime
 {
-	public class DataReader
-	{
-		private readonly IDataReader _dataReader;
+    public class DataReader
+    {
+        private readonly IDataReader _dataReader;
 
-		public DataReader(IDataReader dataReader)
-		{
-			_dataReader = dataReader;
-		}
+        public DataReader(IDataReader dataReader)
+        {
+            _dataReader = dataReader;
+        }
 
-		public IEnumerable<T> ToObjectList<T>() where T : new()
-		{
-			try
-			{
-				var list = new List<T>();
-				var properties = typeof(T).GetProperties();
-				var readerFields = GetFields();
+        public IEnumerable<T> ToObjectList<T>() where T : new()
+        {
+            try
+            {
+                var list = new List<T>();
+                var properties = typeof(T).GetProperties();
+                var readerFields = GetFields();
 
-				bool isDynamic = (typeof(IDynamicMetaObjectProvider).IsAssignableFrom(typeof(T)));
-				IDictionary<string, object> dynamicValues = null;
+                bool isDynamic = (typeof(IDynamicMetaObjectProvider).IsAssignableFrom(typeof(T)));
+                IDictionary<string, object> dynamicValues = null;
 
-				while (_dataReader.Read())
-				{
-					var item = new T();
+                while (_dataReader.Read())
+                {
+                    var item = new T();
 
-					foreach (var field in readerFields)
-					{
-						if (isDynamic)
-						{
-							if (dynamicValues == null)
-							{
-								dynamicValues = (IDictionary<string, object>)item;
-							}
+                    foreach (var field in readerFields)
+                    {
+                        if (isDynamic)
+                        {
+                            if (dynamicValues == null)
+                            {
+                                dynamicValues = (IDictionary<string, object>)item;
+                            }
 
-							dynamicValues[field] = GetSafeValue(_dataReader[field].GetType(), field);
+                            dynamicValues[field] = GetSafeValue(_dataReader[field].GetType(), field);
 
-							continue;
-						}
+                            continue;
+                        }
 
-						var property = properties.FirstOrDefault(p => p.Name.ToUpper() == field.ToUpper());
-						if (property == null || !property.CanWrite) continue;
+                        var property = properties.FirstOrDefault(p => p.Name.ToUpper() == field.ToUpper());
+                        if (property == null || !property.CanWrite) continue;
 
-						try
-						{
-							var safeValue = GetSafeValue(Nullable.GetUnderlyingType(property.PropertyType) ?? property.PropertyType, field);
-							property.SetValue(item, safeValue, null);
-						}
-						catch (FormatException err)
-						{
-							var message = String.Format("Error to convert column {0} to property {1} {2}.{3}",
-								property.Name, property.PropertyType.Name, typeof(T).Name, property.Name);
+                        try
+                        {
+                            var safeValue = GetSafeValue(Nullable.GetUnderlyingType(property.PropertyType) ?? property.PropertyType, field);
+                            property.SetValue(item, safeValue, null);
+                        }
+                        catch (FormatException err)
+                        {
+                            var message = String.Format("Error to convert column {0} to property {1} {2}.{3}",
+                                property.Name, property.PropertyType.Name, typeof(T).Name, property.Name);
 
-							throw new FormatException(message, err);
-						}
-					}
+                            throw new FormatException(message, err);
+                        }
+                    }
 
-					list.Add(item);
-				}
+                    list.Add(item);
+                }
 
-				return list;
-			}
-			finally
-			{
-				_dataReader.Close();
-			}
-		}
+                return list;
+            }
+            finally
+            {
+                _dataReader.Close();
+            }
+        }
 
-		private object GetSafeValue(Type propertyType, string field)
-		{
-			object safeValue = null;
+        private object GetSafeValue(Type propertyType, string field)
+        {
+            object safeValue = null;
 
-			if (propertyType.IsEnum)
-			{
-				var enumValue = Convert.ToInt32(_dataReader[field].ToString());
-				safeValue = Enum.ToObject(propertyType, enumValue);
-			}
-			else
-			{
-				var isNull = _dataReader[field] == null || _dataReader[field] is DBNull;
-				safeValue = (isNull) ? null : Convert.ChangeType(_dataReader[field], propertyType);
-			}
+            if (propertyType.IsEnum)
+            {
+                var enumValue = Convert.ToInt32(_dataReader[field].ToString());
+                safeValue = Enum.ToObject(propertyType, enumValue);
+            }
+            else
+            {
+                var isNull = _dataReader[field] == null || _dataReader[field] is DBNull;
+                safeValue = (isNull) ? null : Convert.ChangeType(_dataReader[field], propertyType);
+            }
 
-			var stringSafeValue = safeValue as String;
-			if (stringSafeValue != null)
-			{
-				safeValue = stringSafeValue.Trim();
-			}
+            var stringSafeValue = safeValue as String;
+            if (stringSafeValue != null)
+            {
+                safeValue = stringSafeValue.Trim();
+            }
 
-			return safeValue;
-		}
+            return safeValue;
+        }
 
-		public IEnumerable<T> ToEnumerable<T>()
-		{
-			try
-			{
-				var list = new List<T>();
-				while (_dataReader.Read())
-				{
-					list.Add(CastTo<T>(_dataReader[0]));
-				}
-				return list;
-			}
-			finally
-			{
-				_dataReader.Close();
-			}
-		}
+        public IEnumerable<T> ToEnumerable<T>()
+        {
+            try
+            {
+                var list = new List<T>();
+                while (_dataReader.Read())
+                {
+                    list.Add(CastTo<T>(_dataReader[0]));
+                }
+                return list;
+            }
+            finally
+            {
+                _dataReader.Close();
+            }
+        }
 
-		public string[] GetFields()
-		{
-			var fields = new String[_dataReader.FieldCount];
-			for (int i = 0; i < _dataReader.FieldCount; i++) fields[i] = _dataReader.GetName(i);
-			return fields;
-		}
+        public string[] GetFields()
+        {
+            var fields = new String[_dataReader.FieldCount];
+            for (int i = 0; i < _dataReader.FieldCount; i++) fields[i] = _dataReader.GetName(i);
+            return fields;
+        }
 
-		public static T CastTo<T>(object value)
-		{
-			if (value is DBNull) return default(T);
-			return (T)Convert.ChangeType(value, typeof(T));
-		}
-	}
+        public static T CastTo<T>(object value)
+        {
+            if (value is DBNull) return default(T);
+            return (T)Convert.ChangeType(value, typeof(T));
+        }
+    }
 }
